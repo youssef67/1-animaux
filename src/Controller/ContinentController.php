befec3738bf04b99708b50e92f89ef4ecb4e48b0
Youssef Moudni <youssefmoudni@macbook-air-de-youssef.home>
2020-05-29 16:35:42 +0200

    mise en place de la totalité des routes

---
+++ src/Controller/ContinentController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Continent;
+use App\Repository\ContinentRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContinentController extends AbstractController
+{
+    /**
+     * @Route("/continents", name="continents")
+     */
+    public function continents(ContinentRepository $repository)
+    {
+        $continents = $repository->findAll();
+
+        return $this->render('continent/continents.html.twig', [
+            'continents' => $continents
+        ]);
+    }
+
+    /**
+     * @Route("/continent/{id}", name="afficherContinent")
+     */
+    public function afficherContinent(Continent $continent)
+    {
+        return $this->render('continent/afficherContinent.html.twig', [
+            'continent' => $continent
+        ]);
+    }
+}
--- src/DataFixtures/AnimalFixtures.php
+++ src/DataFixtures/AnimalFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Animal;
+use App\Entity\Continent;
 use App\Entity\Famille;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -11,20 +12,40 @@ class AnimalFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $c1 = new Famille();
-        $c1->setLibelle("mamimfère")
-            ->setDescription("Animaux vertébrés nourissant leurs petits avec du lait");
+        $c1 = new Continent();
+        $c1->setLibelle('Europe');
         $manager->persist($c1);
-
-        $c2 = new Famille();
-        $c2->setLibelle("reptile")
-            ->setDescription("Animaux vertébrés nourissant qui rampent");
+        
+        $c2 = new Continent();
+        $c2->setLibelle('Amerique');
         $manager->persist($c2);
         
-        $c3 = new Famille();
-        $c3->setLibelle("poissons")
-            ->setDescription("Animaux invertébrés du monde aquatique");
+        $c3 = new Continent();
+        $c3->setLibelle('Asie');
         $manager->persist($c3);
+
+        $c4 = new Continent();
+        $c4->setLibelle('Afrique');
+        $manager->persist($c4);
+        
+        $c5 = new Continent();
+        $c5->setLibelle('Océanie');
+        $manager->persist($c5);
+        
+        $f1 = new Famille();
+        $f1->setLibelle("mamimfère")
+            ->setDescription("Animaux vertébrés nourissant leurs petits avec du lait");
+        $manager->persist($f1);
+
+        $f2 = new Famille();
+        $f2->setLibelle("reptile")
+            ->setDescription("Animaux vertébrés nourissant qui rampent");
+        $manager->persist($f2);
+        
+        $f3 = new Famille();
+        $f3->setLibelle("poissons")
+            ->setDescription("Animaux invertébrés du monde aquatique");
+        $manager->persist($f3);
 
         $a1 = new Animal();
         $a1->setNom('Chien')
@@ -32,7 +53,11 @@ class AnimalFixtures extends Fixture
             ->setImage('chien.png')
             ->setPoid(12)
             ->setDangereux(false)
-            ->setFamille($c1);
+            ->setFamille($f1)
+            ->addContinent($c1)
+            ->addContinent($c2)
+            ->addContinent($c3)
+            ->addContinent($c4);
         $manager->persist($a1);
 
         $a2 = new Animal();
@@ -41,7 +66,11 @@ class AnimalFixtures extends Fixture
             ->setImage('cochon.png')
             ->setPoid(30)
             ->setDangereux(false)
-            ->setFamille($c1);
+            ->setFamille($f1)
+            ->addContinent($c1)
+            ->addContinent($c2)
+            ->addContinent($c3)
+            ->addContinent($c4);
         $manager->persist($a2);
 
         $a3 = new Animal();
@@ -50,7 +79,11 @@ class AnimalFixtures extends Fixture
             ->setImage('serpent.png')
             ->setPoid(3)
             ->setDangereux(true)
-            ->setFamille($c2);
+            ->setFamille($f2)
+            ->addContinent($c1)
+            ->addContinent($c2)
+            ->addContinent($c5)
+            ->addContinent($c3);
         $manager->persist($a3);
 
         $a4 = new Animal();
@@ -59,7 +92,9 @@ class AnimalFixtures extends Fixture
             ->setImage('crocodile.png')
             ->setPoid(110)
             ->setDangereux(true)
-            ->setFamille($c2);
+            ->setFamille($f2)
+            ->addContinent($c2)
+            ->addContinent($c5);
         $manager->persist($a4);
 
         $a5 = new Animal();
@@ -68,7 +103,11 @@ class AnimalFixtures extends Fixture
             ->setImage('requin.png')
             ->setPoid(150)
             ->setDangereux(true)
-            ->setFamille($c3);
+            ->setFamille($f3)
+            ->addContinent($c1)
+            ->addContinent($c2)
+            ->addContinent($c4)
+            ->addContinent($c5);
         $manager->persist($a5);
 
         $manager->flush();
